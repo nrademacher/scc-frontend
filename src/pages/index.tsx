8624e5de4d7c7657297e0bac683c92993cb8a94e
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-05-02 16:10:00 +0200

    chore: replace tailwindcss with bootstrap styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { addApolloState, initializeApollo } from '@/lib/apollo'
 import { gql, useQuery } from '@apollo/client'
+import { Container, Row, Card, Button } from 'react-bootstrap'
+
 
 const helloQuery = gql`
     query Query {
@@ -7,27 +9,43 @@ const helloQuery = gql`
     }
 `
 
-const Home = () => {
-    const { data, loading, error } = useQuery(helloQuery)
+const Home = ({data}: any) => {
+    // const { data, loading, error } = useQuery(helloQuery)
 
-    if (loading) return <p>Loading...</p>
-    if (error) return <p>Oh no... {error.message}</p>
+    // if (loading) return <p>Loading...</p>
+    // if (error) return <p>Oh no... {error.message}</p>
 
-    return <h1 className="grid place-items-center min-h-screen text-7xl">{data.hello}</h1>
+    return (
+    <Container>
+         <Row className="justify-content-md-between">
+            <Card className="sml-card">
+              <Card.Body>
+                <Card.Title>Welcome</Card.Title>
+                <Card.Text>
+                  {data.hello}
+                </Card.Text>
+                <Button variant="primary" href="https://nextjs.org/docs">
+                  More &rarr;
+                </Button>
+              </Card.Body>
+            </Card>
+          </Row>
+    </Container>
+    )
 }
 
 export default Home
 
-// export async function getStaticProps() {
-//   const apolloClient = initializeApollo();
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
 
-//   const { data } = await apolloClient.query({
-//     query: helloQuery,
-//   });
+  const { data } = await apolloClient.query({
+    query: helloQuery,
+  });
 
-//   return addApolloState(apolloClient, {
-//     props: {
-//       data: JSON.parse(JSON.stringify(data)),
-//     },
-//   });
-// }
+  return addApolloState(apolloClient, {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  });
+}
